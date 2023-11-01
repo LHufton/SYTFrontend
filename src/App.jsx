2fd636ff7edb242d0ee1fcd835c613f2726c623e
LHufton <lukehufton@gmail.com>
2023-11-01 02:54:09 -0500

    routing issues fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { CheckSession } from './Services/Auth.js'
+import Nav from './Components/Nav/Nav.jsx'
+import Feed from './Components/Feed/Feed.jsx'
+import Register from './Pages/Register.jsx'
+import SignIn from './Pages/SignIn.jsx'
+import Home from './Pages/Home.jsx'
+import Comments from './Components/Comments/Comments.jsx'
+import Posts from './Components/Posts/Posts.jsx'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
+  const handleLogOut = () => {
+    setUser(null)
+    localStorage.clear()
+  }
+
+  const checkToken = async () => {
+    try {
+      const user = await CheckSession()
+      setUser(user)
+    } catch (error) {
+      console.error('Session validation failed:', error)
+      localStorage.removeItem('token')
+    }
+  }
+
+  const [themeMode, setThemeMode] = useState('light')
+
+  const toggleTheme = () => {
+    setThemeMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'))
+  }
+
+  useEffect(() => {
+    const root = document.documentElement
+    root.classList.remove('light', 'dark')
+    root.classList.add(themeMode)
+  }, [themeMode])
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme')
+    if (storedTheme) {
+      setThemeMode(storedTheme)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('theme', themeMode)
+  }, [themeMode])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className={`App ${themeMode}`}>
+        <header></header>
+        <Nav user={user} handleLogOut={handleLogOut} />
+        <main>
+          <button className="darkButton" onClick={toggleTheme}>
+            Light/Dark Mode
+          </button>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signin" element={<SignIn setUser={setUser} />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/comments" element={<Comments user={user} />} />
+            <Route path="/feed" element={<Feed user={user} />} />
+            <Route path="/posts" element={<Posts user={user} />} />
+          </Routes>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Router>
   )
 }
 
---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const Nav = ({ user, handleLogOut }) => {
+  let userOptions
+  if (user) {
+    userOptions = (
+      <nav className="user-nav-links">
+        <h3>Welcome {user.email}!</h3>
+        <Link to="/feed">Feed</Link>
+        <Link to="/Comments">Comments</Link>
+        <Link to="/Posts">Posts</Link>
+        <Link onClick={handleLogOut} to="/">
+          Sign Out
+        </Link>
+      </nav>
+    )
+  }
+
+  const publicOptions = (
+    <nav className="public-nav-links">
+      <Link to="/">Home</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/signin">Sign In</Link>
+    </nav>
+  )
+
+  return <header>{user ? userOptions : publicOptions}</header>
+}
+
+export default Nav
